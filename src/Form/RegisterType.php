debd7f937cd3f118d3410cd49d8a265e1fd97da5
JeremyGi-Dev <j.gimonet@gmail.com>
2021-01-31 13:05:17 +0100

    modified:   config/packages/twig.yaml 	new file:   migrations/Version20210131114605.php 	new file:   src/Controller/RegisterController.php 	modified:   src/Entity/User.php 	new file:   src/Form/RegisterType.php 	modified:   templates/base.html.twig 	modified:   templates/home/index.html.twig 	new file:   templates/register/index.html.twig

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre e-mail'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe',
+                    'autocomplete' => 'off'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmer votre mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Confirmer votre mot de passe',
+                    'autocomplete' => 'off'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Inscription'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
