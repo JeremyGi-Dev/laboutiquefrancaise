2c3e4064732b407a900b60ae7d31d40df1e50ee8
JeremyGi-Dev <j.gimonet@gmail.com>
2021-02-03 18:44:28 +0100

    modified:   config/packages/security.yaml 	modified:   config/packages/translation.yaml 	modified:   src/Controller/RegisterController.php 	new file:   src/Controller/SecurityController.php 	modified:   src/Form/RegisterType.php 	new file:   src/Security/LoginFormAuthenticator.php 	new file:   templates/security/login.html.twig

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -18,36 +20,46 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre prénom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre nom'
                 ]
             ])
             ->add('email', EmailType::class, [
                 'label' => 'E-mail',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ]),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre e-mail'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique',
                 'label' => 'Mot de passe',
-                'attr' => [
-                    'placeholder' => 'Merci de saisir votre mot de passe',
-                    'autocomplete' => 'off'
-                ]
-            ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmer votre mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Confirmer votre mot de passe',
-                    'autocomplete' => 'off'
-                ]
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe'
+                    ]
+                ],
+                'second_options' => ['label' => 'Confirmez votre mot de passe']
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Inscription'
